aac020fbc7e2eb495bc09eaa503948215d7b3777
gökmen atakan türkmen <34383273+gturkmen3515@users.noreply.github.com>
2023-11-09 13:10:16 +0300

    Delete Vuran_ws/src/dummy_controller directory

--- Vuran_ws/src/dummy_controller/src/dummy_controller_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "dummy_controller/dummy_controller.hpp"
-
-int main(int argc, char** argv)
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DummyController>());
-  rclcpp::shutdown();
-  return 0;
-}
